81546734b4b7184b1a8fe026cb2dfc5eacc4f800
MOHAMedelhennawy <el7ennawy2002@gmail.com>
2024-08-10 15:48:20 +0300

    Add First Task

---
+++ 0x01-ES6_promise/utils.js
@@ -0,0 +1,15 @@
+// utils.js
+export const uploadPhoto = () => {
+    return {
+      status: 200,
+      body: 'photo-profile-1',
+    };
+  };
+  
+  export const createUser = () => {
+    return {
+      firstName: 'Guillaume',
+      lastName: 'Salva',
+    };
+  };
+  
